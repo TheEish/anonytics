7ba62a400e56564beffccb5819f20b623ece8780
Matthew Cox <62405834+Krocostar@users.noreply.github.com>
2020-04-01 23:09:28 -0400

    Modified anonymization file

--- Anonytics Website R Files/anonytics_anonymizeDataFile.r
+++ Anonytics Website R Files/anonytics_anonymizeDataFile.r
@@ -1,62 +1,86 @@
 library(shiny)
-columnNames <- c("Name", "Address")
-dummy <- c("Jimmy John", "123 Carol")
-inputTypes <- c("None" = "none", "Name" = "n", "Address" = "a", "SSN" = "social")
+library(tools)
+library(purrr)
+library(readxl)
+library(shinyalert)
+
 # Define UI
 ui <- fluidPage(
+  useShinyalert(),
   titlePanel("Anonytics"),
-  # Grid Layout
-  fluidRow(column(width = 8, selectInput("template", "Template:",
-                                  c("None" = "none", "Names&Addresses" = "n&a", "Names" = "n", "Addresses" = "a"))
-  )),
-  fluidRow(column(1, checkboxGroupInput("variable", "", columnNames)
-                  ),
-           column(2, selectInput("mapping", "", 
-                                 inputTypes),
-                  selectInput("mapping", "", 
-                              inputTypes)
-           )
-           ),
-  fluidRow(column(width = 8, textInput("saveTemplate", "Save Template", "Enter a template name"),
-                  actionButton("runTemplateSave", "Save")
-                  )
-          ),
-  fluidRow(column(width = 8,
-                  checkboxInput("include", "Include Original File", FALSE),
-                  actionButton("anonymize", "Anonymize Data File")
-                  )
-           ),
-  textOutput("value")
+  fluidRow(
+    column(width = 8, fileInput("inputFile", "Upload a CSV/Excel File",
+      accept = c(
+       "text/csv",
+       "text/comma-separated-values,text/plain",
+       ".csv",
+       ".xlsx"
+      ))
+    )
+  ),
+  
+  fluidRow(
+    column(width = 8, selectInput("template", "Template:", c(
+      "None" = "none",
+      "Names&Addresses" = "n&a",
+      "Names" = "n",
+      "Addresses" = "a"
+    )))
+  ),
+  
+  uiOutput("inputContents"),
+  
+  fluidRow(
+    column(width = 8, textInput("saveTemplate", "Save Template", "Enter a template name"),
+      actionButton("runTemplateSave", "Save")
+    )
+  ),
+  
+  fluidRow(
+    column(width = 8,
+      checkboxInput("include", "Include Original File", FALSE),
+      actionButton("anonymize", "Anonymize Data File")
+    )
+  )
 )
 
-
 # Server function
-server <- function(input, output) {
-  anonymize <- c()
-  value <- renderText({ 
-    selection <- paste(input$variable, collapse = ", ")
-    })
-  
-  for (i in 1:length(columnNames)) {
-    print(columnNames[i])
-    if (columnNames[i] %in% value) {
-      anonymize[i] <- TRUE
-    }
-    else {
-      anonymize[i] <- FALSE
-    }
-  }
-  
-  if (length(columnNames) == length(dummy)) {
-    for (i in 1: length(columnNames)) {
-      if (anonymize[i]) {
-        dummy[i] <- "Potato"
+server <- function(input, output, session)
+{
+  data <- eventReactive(input$inputFile, {
+    inputFile <- input$inputFile
+    
+    tryCatch({
+      if (tolower(file_ext(inputFile$datapath)) == "xlsx")
+      {
+        read_xlsx(inputFile$datapath)
       }
-    }
-  }
+      else if (tolower(file_ext(inputFile$datapath)) == "csv")
+      {
+        read.csv(inputFile$datapath)
+      }
+      else
+      {
+        shinyalert("Error", "Invalid file type.", type = "error")
+      }
+    }, error = function(e)
+    {
+      shinyalert("Error", "An error occurred during file upload.", type = "error")
+    })
+  })
   
-  anonymizeTest <- data.frame(columnNames, anonymize)
-  write.csv(anonymizeTest, "D:\\College Stuff\\Spring 2020\\CIS 499\\Anonytics Website R Files\\testAnon.csv", row.names = FALSE)
+  vars <- reactive(colnames(data()))
+  
+  output$inputContents <- renderUI(
+    map(vars(), ~ fluidRow(
+      checkboxInput(paste(.x, "Checked"), .x, TRUE),
+      selectInput(paste(.x, "Selected"), "Dataset:", c(
+        "Test 1" = "t1",
+        "Test 2" = "t2",
+        "Test 3" = "t3"
+      ))
+    ))
+  )
 }
 
 # Run the application
