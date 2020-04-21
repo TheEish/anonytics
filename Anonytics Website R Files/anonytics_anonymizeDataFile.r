36321fbc3ef65586645cb196f92fc1e8e4013d04
Matthew Cox <62405834+Krocostar@users.noreply.github.com>
2020-04-21 05:12:31 -0400

    Finished anonymization method

--- Anonytics Website R Files/anonytics_anonymizeDataFile.r
+++ Anonytics Website R Files/anonytics_anonymizeDataFile.r
@@ -4,9 +4,12 @@ library(purrr)
 library(readxl)
 library(shinyalert)
 
-datasetTypes <- c("Name" = "Name", "Address" = "Address", "SSN" = "SSN")
+datasetData <- c()
 
-templatesDir <- "C:\\Users\\Matthew\\Documents\\_University\\Anonytics Website R Files\\Templates\\"
+datasetsDir <- "Datasets/"
+templatesDir <- "Templates/"
+
+datasetTypes <- gsub("\\.txt$", "", list.files(datasetsDir, "\\.txt$"))
 templateNames <- gsub("\\.csv$", "", list.files(templatesDir, "\\.csv$"))
 
 displayTemplate <- function(input, output, session, vars)
@@ -25,22 +28,15 @@ displayTemplate <- function(input, output, session, vars)
       for (i in 1:length(vars()))
       {
         columnName <- vars()[i]
-        print(vars()[columnName])
         if (columnName %in% templateData[, 1])
         {
-          # checkedVar[columnName] <- TRUE
-          # selectedVar[columnName] <- templateData[grep(columnName, templateData[, 1]), 2]
-          
-          print(paste(templateData[grep(columnName, templateData[, 1]), 2]))
-          print(paste(selectedVar[columnName]))
-          # updateCheckboxInput(session, paste(columnName, "Checked"), value = TRUE)
-          # updateSelectInput(session, paste(columnName, "Selected"), selected = templateData[index, 2])
+          checkedVar[columnName] <- TRUE
+          selectedVar[columnName] <- paste(templateData[grep(columnName, templateData[, 1]), 2])
         }
         else
         {
-          # checkedVar[columnName] <- FALSE
-          # selectedVar[columnName] <- input[[paste(columnName, "Selected")]]
-          # updateCheckboxInput(session, paste(columnName, "Checked"), value = FALSE)
+          checkedVar[columnName] <- FALSE
+          selectedVar[columnName] <- input[[paste(columnName, "Selected")]]
         }
       }
       
@@ -99,6 +95,15 @@ ui <- fluidPage(
 # Server function
 server <- function(input, output, session)
 {
+  datasetFiles <- list.files(path=datasetsDir, pattern="*.txt", full.names=TRUE, recursive=FALSE)
+  for (file in datasetFiles)
+  {
+    name <- sub("\\.txt$", "", basename(file))
+    data <- read.delim(file, comment.char="#")
+    
+    datasetData[name] <- data
+  }
+  
   observe({
     updateSelectInput(session, "template", label = "Select template", choices = c("None", templateNames))
   })
@@ -109,11 +114,11 @@ server <- function(input, output, session)
     tryCatch({
       if (tolower(file_ext(inputFile$datapath)) == "xlsx")
       {
-        read_xlsx(inputFile$datapath)
+        read.xlsx(inputFile$datapath, stringsAsFactors=FALSE)
       }
       else if (tolower(file_ext(inputFile$datapath)) == "csv")
       {
-        read.csv(inputFile$datapath)
+        read.csv(inputFile$datapath, stringsAsFactors=FALSE)
       }
       else
       {
@@ -139,14 +144,26 @@ server <- function(input, output, session)
   })
   
   observeEvent(input$anonymize, {
+    newData <- data.frame(data())
     for (i in 1:length(vars()))
     {
-      name <- vars()[i]
-      if (input[[paste(name, "Checked")]] == TRUE)
+      columnName <- vars()[i]
+      if (input[[paste(columnName, "Checked")]] == TRUE)
       {
-        print(input[[paste(name, "Selected")]])
+        dataset <- input[[paste(columnName, "Selected")]]
+        dataFromDataset <- datasetData[[dataset]]
+        
+        numberOfRows <- length(data()[, i])
+        datasetLength <- length(dataFromDataset)
+        
+        for (n in 1:numberOfRows)
+        {
+          newValue <- dataFromDataset[sample(1:datasetLength, 1)]
+          newData[n, i] <- paste(newValue)
+        }
       }
     }
+    write.csv(newData, "test.csv", row.names=FALSE)
   })
   
   observeEvent(input$runTemplateSave, {
@@ -166,7 +183,7 @@ server <- function(input, output, session)
     templateFrame <- data.frame(columnNames, columnDatasets)
     templateName <- input$saveTemplate
     filePath <- paste(templatesDir, paste(templateName, ".csv", sep=""), sep="")
-    write.csv(templateFrame, filePath, row.names = FALSE)
+    write.csv(templateFrame, filePath, row.names=FALSE)
   })
 }
 
